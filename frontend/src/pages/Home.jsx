b7e1caebb179727680acc228a5c83d0a12392593
kateboni <kateboni@outlook.com>
2025-02-23 22:24:46 +0200

    Initial commit

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,15 @@
+import React, { useEffect, useState } from 'react';
+
+const Home = () => {
+
+    return (
+    <div>
+        <div>
+            <h1>Explore the museums of your city</h1>
+        </div>
+    </div>
+
+);
+};
+
+export default Home;
